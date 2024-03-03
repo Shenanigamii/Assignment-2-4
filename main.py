ac2c0ef3c428fef981d493e82b4eee82a1e85beb
Shenanigamii <159212161+Shenanigamii@users.noreply.github.com>
2024-03-03 23:50:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# input statements
+salary = float(input("Enter your salary: "))
+numdependents = float(input("Enter number of dependents: "))
+
+# calculate taxes here
+
+#dependentDeduction comes out of gross income before taxable income is calculated
+  #example in assignment didn't calculate taxes correctly
+  #if example numbers are needed, remove " - dependentDeduction" from associated lines
+
+#Deduction for dependents
+dependentDeduction = .025 * numdependents * salary
+#State Tax withholding at 6.5%
+stateTaxRate = .065
+stateTax = (salary - dependentDeduction) * stateTaxRate
+#Federal Tax withholding at 28%
+federalTaxRate = .28
+federalTax = (salary - dependentDeduction) * federalTaxRate
+#Total Withheld
+totalWithholding = stateTax + federalTax + dependentDeduction
+#Total Takehome
+takeHomePay = salary - totalWithholding
+
+# output statements
+print("State Tax: $" + str(stateTax))
+print("Federal Tax: $" + str(federalTax))
+print("Dependents: $" + str(dependentDeduction))
+print("Salary: $" + str(salary))
+print("Take Home Pay: $" + str(takeHomePay))
